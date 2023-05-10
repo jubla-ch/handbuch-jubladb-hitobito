3091ecfab44a617b2486051b067a885e7eddf6fb
richardjubla <95908537+richardjubla@users.noreply.github.com>
2023-05-10 09:06:10 +0200

    Update conf.py

--- conf.py
+++ conf.py
@@ -19,9 +19,9 @@
 
 # -- Project information -----------------------------------------------------
 
-project = u'Hitobito'
-copyright = u'2021, hitobito AG'
-author = u'Hitobito Contritubtors'
+project = u'handbuch-jubladb-hitobito'
+copyright = u'2023, jubla Schweiz'
+author = u'jubla'
 
 # The short X.Y version
 version = u''
@@ -117,7 +117,7 @@ html_static_path = ['_static']
 # -- Options for HTMLHelp output ---------------------------------------------
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'hitobitodoc'
+htmlhelp_basename = 'jubladb-handbuch'
 
 
 # -- Options for LaTeX output ------------------------------------------------
@@ -144,7 +144,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'hitobito.tex', u'hitobito Documentation',
+    (master_doc, 'jubladb-handbuch.tex', u'jubla.db Benutzerhandbuch',
      u'Roland', 'manual'),
 ]
 
@@ -154,7 +154,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'hitobito', u'hitobito Documentation',
+    (master_doc, 'jubladb-handbuch', u'jubla.db Benutzerhandbuch',
      [author], 1)
 ]
 
@@ -165,8 +165,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'hitobito', u'hitobito Documentation',
-     author, 'hitobito', 'One line description of project.',
+    (master_doc, 'jubladb-handbuch', u'jubla.db Benutzerhandbuch',
+     author, 'jubladb-handbuch', 'Benutzerhandbuch der nationalen Datenbank «jubla.db» von Jungwacht Blauring.',
      'Miscellaneous'),
 ]
 
