d9bade066f4d6cefe81a8c4aed8daaa0f137571a
marcjubla <marc.hoesli@jubla.ch>
2024-09-04 12:06:13 +0200

    Theme-Erweiterung

--- conf.py
+++ conf.py
@@ -71,7 +71,7 @@ language = u'de'
 exclude_patterns = [u'_build', 'Thumbs.db', '.DS_Store']
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'sphinx'
+pygments_style = "PYGMENTS_THEME"
 
 
 # -- Options for HTML output -------------------------------------------------
