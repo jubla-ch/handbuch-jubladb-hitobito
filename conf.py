1cf155ec691272e73806a17cb0b8998d3ee3a2e4
marcjubla <marc.hoesli@jubla.ch>
2024-09-10 15:23:17 +0200

    restore extension

--- conf.py
+++ conf.py
@@ -39,7 +39,8 @@ release = u''
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-   "sphinxawesome.deprecated",
+    'sphinx.ext.todo',
+    'sphinx.ext.ifconfig',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
