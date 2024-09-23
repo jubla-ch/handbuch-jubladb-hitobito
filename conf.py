2ffd0cc7ed95a6d784b77f577a389605f0d52d29
marcjubla <marc.hoesli@jubla.ch>
2024-09-23 11:19:38 +0200

    html_css_files Create

--- conf.py
+++ conf.py
@@ -108,6 +108,11 @@ html_theme_options = {
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
+
+html_css_files = ["https://primer.style/foundations/icons/"]
+
+
+
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
 #
