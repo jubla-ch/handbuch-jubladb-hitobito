8a6162de385e31817b86b112d759cced2e4168ed
marcjubla <marc.hoesli@jubla.ch>
2024-09-24 15:04:04 +0200

    awesome_headerlinks

--- conf.py
+++ conf.py
@@ -95,6 +95,7 @@ html_theme_options = {
     'display_version': True,
     'prev_next_buttons_location': 'bottom',
     'style_external_links': False,
+    'awesome_headerlinks': True,
     # Toc options
     'collapse_navigation': True,
     'sticky_navigation': True,
