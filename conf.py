bbe2dad49990d0686bf804d4df5b0295278921b3
marcjubla <marc.hoesli@jubla.ch>
2024-09-24 16:43:18 +0200

    Theme Option Settings

--- conf.py
+++ conf.py
@@ -95,8 +95,10 @@ from dataclasses import asdict
 from sphinxawesome_theme import ThemeOptions
 
 theme_options = ThemeOptions(
-   show_breadcrumbs=True,
-   show_prev_next= True
+   show_prev_next = True,
+   awesome_headerlinks = True,
+   globaltoc_includehidden = True,
+   breadcrumbs_separator = '>'
 )
 
 html_theme_options = {
@@ -104,9 +106,6 @@ html_theme_options = {
     'display_version': True,
     'prev_next_buttons_location': 'bottom',
     'style_external_links': False,
-    'awesome_headerlinks': True,
-    'breadcrumbs_separator': '>',
-    'show_prev_next': True,
     # Toc options
     'collapse_navigation': True,
     'sticky_navigation': True,
